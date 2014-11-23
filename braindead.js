39fc0e6722d791e4003c6590436a3a3c01b401ec
ronaldlee <ronaldylee@gmail.com>
2014-11-23 11:16:51 -0800

    fade inout

--- braindead.js
+++ braindead.js
@@ -2,35 +2,43 @@
 //document.body.style.backgroundColor="blue";
 
 $(document).ready(function(){
-    var content = 
-      "<div id='braindead'> \
-         <ul> \
-           <li><a href='#tabs-1'>Quiz</a></li> \
-           <li><a href='#tabs-2'>Others</a></li> \
-           <li><a href='#tabs-3'>Config</a></li> \
-         </ul> \
-         <div id='tabs-1' style=''> \
-           <form style='display:inline-block; '> \
-             <div style='float:left; width:80%; '> \
-               <table style='width:450px; height:100%'> \
-                 <tr style='width:100%'><td style='text-align:left'><font color='white'>Q:</font><input type='text' value='Type Quiz Here' style='width:300px'/> \
-                                                                    <font color='white''>?</font></td></tr>  \
-                 <tr style='width:100%'><td style='text-align:left'><font color='white'>A:</font><input type='text' value='Type Answer Here' style='width:300px'/></td></tr> \
-               </table> \
+    if ($('#braindead').length) {
+        $("#braindead").fadeOut( "fast", function() {
+            $('#braindead').remove();
+        });
+    }
+    else {
+        var content = 
+          "<div id='braindead' style='display:none'> \
+             <ul> \
+               <li><a href='#tabs-1'>Quiz</a></li> \
+               <li><a href='#tabs-2'>Others</a></li> \
+               <li><a href='#tabs-3'>Config</a></li> \
+             </ul> \
+             <div id='tabs-1' style=''> \
+               <form style='display:inline-block; '> \
+                 <div style='float:left; width:80%; '> \
+                   <table style='width:450px; height:100%'> \
+                     <tr style='width:100%'><td style='text-align:left'><font color='white'>Q:</font><input type='text' value='Type Quiz Here' style='width:300px'/> \
+                                                                        <font color='white''>?</font></td></tr>  \
+                     <tr style='width:100%'><td style='text-align:left'><font color='white'>A:</font><input type='text' value='Type Answer Here' style='width:300px'/></td></tr> \
+                   </table> \
+                 </div> \
+                 <div style='float:left; width:20%; height:80px'> \
+                   <input type='submit' value='Add'/> \
+                 </div> \
+               </form>\
              </div> \
-             <div style='float:left; width:20%; height:80px'> \
-               <input type='submit' value='Add'/> \
+             <div id='tabs-2'> \
              </div> \
-           </form>\
-         </div> \
-         <div id='tabs-2'> \
-         </div> \
-         <div id='tabs-3'> \
-         </div> \
-      </div>";
-    $('body').append(content);
+             <div id='tabs-3'> \
+             </div> \
+          </div>";
+        $('body').append(content);
 
-    $( "#braindead" ).tabs();
+        $("#braindead").tabs();
+        $("#braindead").fadeIn( "fast");
+    }
 
 //    $('#braindead').load('popup.html');
 });
