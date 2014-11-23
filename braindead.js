4557e215bd9ce29a5db34f06613003be6e890e53
ronaldlee <ronaldylee@gmail.com>
2014-11-22 18:54:41 -0800

    first commit

---
+++ braindead.js
@@ -0,0 +1,39 @@
+
+//document.body.style.backgroundColor="blue";
+
+$(document).ready(function(){
+    var content = 
+      "<div id='braindead'> \
+         <ul> \
+           <li><a href='#tabs-1'>Quiz</a></li> \
+           <li><a href='#tabs-2'>Others</a></li> \
+           <li><a href='#tabs-3'>Config</a></li> \
+         </ul> \
+         <div id='tabs-1' style=''> \
+           <form style='display:inline-block; '> \
+             <div style='float:left; width:80%; '> \
+               <table style='width:450px; height:100%'> \
+                 <tr style='width:100%'><td style='text-align:left'><font color='white'>Q:</font><input type='text' value='Type Quiz Here' style='width:300px'/> \
+                                                                    <font color='white''>?</font></td></tr>  \
+                 <tr style='width:100%'><td style='text-align:left'><font color='white'>A:</font><input type='text' value='Type Answer Here' style='width:300px'/></td></tr> \
+               </table> \
+             </div> \
+             <div style='float:left; width:20%; height:80px'> \
+               <input type='submit' value='Add'/> \
+             </div> \
+           </form>\
+         </div> \
+         <div id='tabs-2'> \
+         </div> \
+         <div id='tabs-3'> \
+         </div> \
+      </div>";
+    $('body').append(content);
+
+    $( "#braindead" ).tabs();
+
+//    $('#braindead').load('popup.html');
+});
+
+//$('body').append('<div>BOOO</div>');
+
---
+++ main.js
@@ -0,0 +1,32 @@
+
+/*
+var Braindead = {
+
+  showInputPanel: function() {
+    console.log("calling show input panel");
+    $('body').append('<div>BOOO</div>');
+  },
+};
+
+// Run our kitten generation script as soon as the document's DOM is ready.
+document.addEventListener('DOMContentLoaded', function () {
+  alert('yoo');
+  //Braindead.showInputPanel();
+});
+*/
+
+//chrome.tabs.executeScript(null, {file: "jquery.min.js"});
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  // No tabs or host permissions needed!
+  chrome.tabs.insertCSS(null, {file: "jquery-ui.css"});
+  chrome.tabs.insertCSS(null, {file: "braindead.css"});
+  chrome.tabs.executeScript(null, {file: "jquery.min.js"});
+  chrome.tabs.executeScript(null, {file: "jquery-ui.min.js"});
+  chrome.tabs.executeScript(null, {file: "braindead.js"});
+  /*
+  chrome.tabs.executeScript({
+    code: 'document.body.style.backgroundColor="red"'
+  });
+  */
+});
