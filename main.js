cbe27412020d1d5da5b5eed3321f0020c0e85883
ronaldlee <ronaldylee@gmail.com>
2014-11-27 15:14:49 -0800

    fix layout

--- main.js
+++ main.js
@@ -19,11 +19,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
 chrome.browserAction.onClicked.addListener(function(tab) {
   // No tabs or host permissions needed!
-  chrome.tabs.insertCSS(null, {file: "jquery-ui.css"});
+  chrome.tabs.insertCSS(null, {file: "libs/jquery-ui.css"});
 //  chrome.tabs.insertCSS(null, {file: "stylesheets\braindead.css"});
   chrome.tabs.insertCSS(null, {file: "stylesheets/braindead.css"});
-  chrome.tabs.executeScript(null, {file: "jquery.min.js"});
-  chrome.tabs.executeScript(null, {file: "jquery-ui.min.js"});
+  chrome.tabs.executeScript(null, {file: "libs/jquery.min.js"});
+  chrome.tabs.executeScript(null, {file: "libs/jquery-ui.min.js"});
+  chrome.tabs.executeScript(null, {file: "libs/jquery.loadTemplate-1.4.5.min.js"});
   chrome.tabs.executeScript(null, {file: "braindead.js"});
   /*
   chrome.tabs.executeScript({
