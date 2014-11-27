fcd4f4b59cf772e8fbcf3c657328d8d78f7d8c3d
ronaldlee <ronaldylee@gmail.com>
2014-11-27 11:41:35 -0800

    more stuff

--- braindead.js
+++ braindead.js
@@ -16,18 +16,24 @@ $(document).ready(function(){
                <li><a href='#tabs-3'>Config</a></li> \
              </ul> \
              <div id='tabs-1' class='tab-content'> \
-               <form style='display:inline-block; '> \
-                 <div id='quiz-content-panel'> \
-                   <table id='quiz-table'> \
-                     <tr class='tb-row'><td class='tb-data'><font color='white'>Q:</font><input type='text' value='Type Quiz Here' style='width:300px'/> \
-                                                                        <font color='white''>?</font></td></tr>  \
-                     <tr class='tb-row'><td class='tb-data'><font color='white'>A:</font><input type='text' value='Type Answer Here' style='width:300px'/></td></tr> \
-                   </table> \
-                 </div> \
-                 <div id='quiz-add-panel'> \
-                   <input type='submit' value='Add'/> \
-                 </div> \
-               </form>\
+               <div id='quiz-uppper-panel'> \
+                 <form> \
+                   <div id='quiz-content-panel'> \
+                     <table id='quiz-table'> \
+                       <tr class='tb-row'><td class='tb-data'><font color='white'>Q:&nbsp;</font><input type='text' value='Type Quiz Here' style='width:300px'/> \
+                                                                          <font color='white''>?</font></td></tr>  \
+                       <tr class='tb-row'><td class='tb-data'><font color='white'>A:&nbsp;</font><input type='text' value='Type Answer Here' style='width:300px'/></td></tr> \
+                     </table> \
+                   </div> \
+                   <div id='quiz-add-panel'> \
+                     <input type='submit' value='Add'/> \
+                   </div> \
+                 </form>\
+               </div> \
+               <hr style='width:100%'/> \
+               <div id='quiz-bottom-panel'> \
+               (Display questions just added, and click on the link will expose answers)\
+               </div> \
              </div> \
              <div id='tabs-2' class='tab-content'> \
              </div> \
