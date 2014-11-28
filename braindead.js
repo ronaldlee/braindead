d4acc9083f7f6ad0c63374713cafa138a5132437
ronaldlee <ronaldylee@gmail.com>
2014-11-27 17:21:42 -0800

    stuff

--- braindead.js
+++ braindead.js
@@ -10,16 +10,20 @@ $(document).ready(function(){
     else {
         var content = 
           "<div id='braindead'> \
-             <ul> \
-               <li><a href='#braindead-tabs-1'>Quiz</a></li> \
-               <li><a href='#braindead-tabs-2'>Others</a></li> \
-               <li><a href='#braindead-tabs-3'>Config</a></li> \
-             </ul> \
-             <div id='braindead-tabs-1' class='tab-content'> \
+             <div id='braindead-main'> \
+               <ul> \
+                 <li><a href='#braindead-tabs-1'>Quiz</a></li> \
+                 <li><a href='#braindead-tabs-2'>Others</a></li> \
+                 <li><a href='#braindead-tabs-3'>Config</a></li> \
+               </ul> \
+               <div id='braindead-tabs-1' class='tab-content'> \
+               </div> \
+               <div id='braindead-tabs-2' class='tab-content'> \
+               </div> \
+               <div id='braindead-tabs-3' class='tab-content'> \
+               </div> \
              </div> \
-             <div id='braindead-tabs-2' class='tab-content'> \
-             </div> \
-             <div id='braindead-tabs-3' class='tab-content'> \
+             <div id='braindead-bottom'> \
              </div> \
           </div>";
         $('body').append(content);
@@ -32,10 +36,14 @@ $(document).ready(function(){
         $('#braindead').loadTemplate(chrome.extension.getURL("/templates/braindead_main.html"));
         */
         $('#braindead-tabs-1').loadTemplate(chrome.extension.getURL("/templates/braindead_quiz_tab.html"));
+        $('#braindead-bottom').loadTemplate(chrome.extension.getURL("/templates/braindead_bottom.html"));
 
-        $("#braindead").tabs();
         $("#braindead").draggable();
         $("#braindead").fadeIn("fast");
+
+        $("#braindead-main").tabs();
+
+//        $('#braindead-logo-img').attr('src', chrome.extension.getURL("/images/braindeadlogo.png"));
     }
 
 //    $('#braindead').load('popup.html');
