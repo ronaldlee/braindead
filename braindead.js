32ba651102b78857c9c199dd34db7972d0670eba
ronaldlee <ronaldylee@gmail.com>
2014-11-27 16:15:16 -0800

    templating

--- braindead.js
+++ braindead.js
@@ -11,40 +11,31 @@ $(document).ready(function(){
         var content = 
           "<div id='braindead'> \
              <ul> \
-               <li><a href='#tabs-1'>Quiz</a></li> \
-               <li><a href='#tabs-2'>Others</a></li> \
-               <li><a href='#tabs-3'>Config</a></li> \
+               <li><a href='#braindead-tabs-1'>Quiz</a></li> \
+               <li><a href='#braindead-tabs-2'>Others</a></li> \
+               <li><a href='#braindead-tabs-3'>Config</a></li> \
              </ul> \
-             <div id='tabs-1' class='tab-content'> \
-               <div id='quiz-uppper-panel'> \
-                 <form> \
-                   <div id='quiz-content-panel'> \
-                     <table id='quiz-table'> \
-                       <tr class='tb-row'><td class='tb-data'><font color='white'>Q:&nbsp;</font><input type='text' value='Type Quiz Here' class='quiz-input'/> \
-                                                                          <font color='white''>?</font></td></tr>  \
-                       <tr class='tb-row'><td class='tb-data'><font color='white'>A:&nbsp;</font><input type='text' value='Type Answer Here' class='quiz-input'/></td></tr> \
-                     </table> \
-                   </div> \
-                   <div id='quiz-add-panel'> \
-                     <input type='submit' value='Add'/> \
-                   </div> \
-                 </form>\
-               </div> \
-               <hr style='width:100%'/> \
-               <div id='quiz-bottom-panel'> \
-               (Display questions just added, and click on the link will expose answers)\
-               </div> \
+             <div id='braindead-tabs-1' class='tab-content'> \
              </div> \
-             <div id='tabs-2' class='tab-content'> \
+             <div id='braindead-tabs-2' class='tab-content'> \
              </div> \
-             <div id='tabs-3' class='tab-content'> \
+             <div id='braindead-tabs-3' class='tab-content'> \
              </div> \
           </div>";
         $('body').append(content);
 
-        $("#braindead").draggable();
+        //$('body').loadTemplate("chrome-extension-resource://templates/braindead_main.html");
+        
+      /*
+        var content = "<div id='braindead'></div>";
+        $('body').append(content);
+        $('#braindead').loadTemplate(chrome.extension.getURL("/templates/braindead_main.html"));
+        */
+        $('#braindead-tabs-1').loadTemplate(chrome.extension.getURL("/templates/braindead_quiz_tab.html"));
+
         $("#braindead").tabs();
-        $("#braindead").fadeIn( "fast");
+        $("#braindead").draggable();
+        $("#braindead").fadeIn("fast");
     }
 
 //    $('#braindead').load('popup.html');
