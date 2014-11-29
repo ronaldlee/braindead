dd557fcc720bd9680db55f582a268026b9feddce
ronaldlee <ronaldylee@gmail.com>
2014-11-28 22:24:37 -0800

    stuff

--- braindead_main.js
+++ braindead_main.js
@@ -1,6 +1,22 @@
 
-$('#braindead-tabs-1').loadTemplate(chrome.extension.getURL("/templates/braindead_quiz_tab.html"));
-$('#braindead-bottom').loadTemplate(chrome.extension.getURL("/templates/braindead_bottom.html"));
 
-$("#braindead-main").tabs();
+$( document ).ready(function() {
+  $('#braindead-tabs-1').loadTemplate(chrome.extension.getURL("/templates/braindead_quiz_tab.html"));
+
+  $('#braindead-bottom').loadTemplate(chrome.extension.getURL("/templates/braindead_bottom.html"), {}, 
+    { 
+      success: function() {
+        $('#quiz-add-button').on('click', 
+          function() { 
+            console.log('clicked'); 
+
+          }
+        );
+      }
+    }
+  );
+
+  $("#braindead-main").tabs();
+
+});
 
