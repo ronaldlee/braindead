8d9a252d66205e3fe71180f327df002fef92d16c
ronaldlee <ronaldylee@gmail.com>
2014-11-30 07:53:48 -0800

    stuff

--- braindead.js
+++ braindead.js
@@ -1,62 +1,24 @@
-
-//document.body.style.backgroundColor="blue";
+//content script
 
 $(document).ready(function(){
+
     if ($('#braindead-iframe').length) {
         $("#braindead-iframe").fadeOut( "fast", function() {
             $('#braindead-iframe').remove();
         });
     }
     else {
-      /*
-        var content = 
-          "<div id='braindead'> \
-             <div id='braindead-main'> \
-               <ul> \
-                 <li><a href='#braindead-tabs-1'>Quiz</a></li> \
-                 <li><a href='#braindead-tabs-2'>Others</a></li> \
-                 <li><a href='#braindead-tabs-3'>Config</a></li> \
-               </ul> \
-               <div id='braindead-tabs-1' class='tab-content'> \
-               </div> \
-               <div id='braindead-tabs-2' class='tab-content'> \
-               </div> \
-               <div id='braindead-tabs-3' class='tab-content'> \
-               </div> \
-             </div> \
-             <div id='braindead-bottom'> \
-             </div> \
-          </div>";
-          */
+      console.log("read config3: " + config);
+
         var content = "<iframe frameBorder='0' scrolling='no' id='braindead-iframe' src='" + chrome.extension.getURL("/templates/braindead_main.html") + "' />";
 
         $('html').append(content);
 
-        //$('body').loadTemplate("chrome-extension-resource://templates/braindead_main.html");
-        
-      /*
-        var content = "<div id='braindead'></div>";
-        $('body').append(content);
-        $('#braindead').loadTemplate(chrome.extension.getURL("/templates/braindead_main.html"));
-        */
-        /*
-        $('#braindead-tabs-1').loadTemplate(chrome.extension.getURL("/templates/braindead_quiz_tab.html"));
-        $('#braindead-bottom').loadTemplate(chrome.extension.getURL("/templates/braindead_bottom.html"));
-
-        $("#braindead").draggable();
-        $("#braindead").fadeIn("fast");
-
-        $("#braindead-main").tabs();
-        */
-
         $("#braindead-iframe").draggable();
         $("#braindead-iframe").fadeIn("fast");
 
-//        $('#braindead-logo-img').attr('src', chrome.extension.getURL("/images/braindeadlogo.png"));
     }
 
-//    $('#braindead').load('popup.html');
 });
 
-//$('body').append('<div>BOOO</div>');
 
--- braindead_main.js
+++ braindead_main.js
@@ -1,20 +1,23 @@
 
 
 $( document ).ready(function() {
-  $('#braindead-tabs-1').loadTemplate(chrome.extension.getURL("/templates/braindead_quiz_tab.html"));
 
-  $('#braindead-bottom').loadTemplate(chrome.extension.getURL("/templates/braindead_bottom.html"), {}, 
+
+  $('#braindead-tabs-1').loadTemplate(chrome.extension.getURL("/templates/braindead_quiz_tab.html"), {},
     { 
       success: function() {
         $('#quiz-add-button').on('click', 
           function() { 
-            console.log('clicked'); 
+            console.log('clicked4: tab: ' + config); 
+
 
           }
         );
       }
     }
   );
+
+  $('#braindead-bottom').loadTemplate(chrome.extension.getURL("/templates/braindead_bottom.html"));
 
   $("#braindead-main").tabs();
 
