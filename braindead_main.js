a45814f6fd4611e8e43662c3d4f2408372d725e0
ronaldlee <ronaldylee@gmail.com>
2014-11-30 19:16:56 -0800

    add login and Parse

--- braindead_main.js
+++ braindead_main.js
@@ -1,34 +1,55 @@
+Parse.initialize("Ga3gF3LIgw8nSXtygi7fodmlehYPT7vc5gdyPnuz", "n3TQpqA2CqUXBSGysKHDQZGENIiy8SYOcpqRpB6j");
+
+/*
+var TestObject = Parse.Object.extend("TestObject");
+var testObject = new TestObject();
+testObject.save({foo: "bar"}).then(function(object) {
+    alert("yay! it worked");
+});
+*/
 
 
 $( document ).ready(function() {
+  var isTest = false;
 
-  var params = location.href.split('?')[1].split('&');
-  data = {};
-  for (x in params)
-  {
-     data[params[x].split('=')[0]] = params[x].split('=')[1];
-  }
+  var currentUser = Parse.User.current();
+  if (currentUser || isTest) {
+    $('#braindead-main-container').show();
 
-  var tabUrl;
-  if (data['tabUrl']) {
-    tabUrl = data['tabUrl'];
-  }
-
-  $('#braindead-tabs-1').loadTemplate(chrome.extension.getURL("/templates/braindead_quiz_tab.html"), {},
+    var params = location.href.split('?')[1].split('&');
+    data = {};
+    for (x in params)
     {
-      success: function() {
-        $('#quiz-add-button').on('click', 
-          function() { 
-            console.log('clicked5: taburl: ' + tabUrl); 
-          }
-        );
-      }
+       data[params[x].split('=')[0]] = params[x].split('=')[1];
     }
-  );
 
-  $('#braindead-bottom').loadTemplate(chrome.extension.getURL("/templates/braindead_bottom.html"));
+    var tabUrl;
+    if (data['tabUrl']) {
+      tabUrl = data['tabUrl'];
+    }
 
-  $("#braindead-main").tabs();
+    $('#braindead-tabs-1').loadTemplate(chrome.extension.getURL("/templates/braindead_quiz_tab.html"), {},
+      {
+        success: function() {
+          $('#quiz-add-button').on('click', 
+            function() { 
+              console.log('clicked5: taburl: ' + tabUrl); 
+            }
+          );
+        }
+      }
+    );
+
+    $('#braindead-bottom').loadTemplate(chrome.extension.getURL("/templates/braindead_bottom.html"));
+
+    $("#braindead-main").tabs();
+
+  } else {
+    // show the signup or login page
+    
+    $('#braindead-login').show();
+  }
+
 
 });
 
